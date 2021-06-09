39a9d8212350e430cbb14c0f3c39e5f449876f60
mattiyee <mattiyee610@gmail.com>
2021-06-08 23:04:00 -0400

    initial commit

---
+++ IterationExercise/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace IterationExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 15; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+        }
+    }
+}
